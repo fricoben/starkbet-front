15c41ed11a6d7689763b240885a1b7312c1a473d
OxBonaparte <>
2022-07-09 14:39:06 +0200

    fix: text build without .env

--- hooks/starknetId.js
+++ hooks/starknetId.js
@@ -4,6 +4,7 @@ import starknet_id_abi from "../abi/starknet_id_abi.json";
 export function useStarknetIdContract() {
   return useContract({
     abi: starknet_id_abi,
-    address: process.env.NEXT_PUBLIC_CONTRACT,
+    address:
+      "0x033233531959c1da39c28daf337e25e2deadda80ce988290306ffabcd735ccbd",
   });
 }
