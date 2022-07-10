1ff4d366622f71c7af48dd664e1a26c5bf54cad2
OxBonaparte <>
2022-07-10 15:17:20 +0200

    fix: add nft id in number

--- pages/discord.js
+++ pages/discord.js
@@ -164,7 +164,7 @@ export default function Discord() {
         body: JSON.stringify({
           reference: reference,
           type: "discord",
-          nftid: tokenId,
+          nftid: Number(tokenId),
         }),
       };
 
